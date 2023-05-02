406da32fd1dfb1830342f8cf7216aa3226e86436
punvp2000 <vorawut.per@gmail.com>
2023-05-02 18:09:42 +0700

    [feat] App bar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-app>
+    <v-navigation-drawer expand-on-hover rail color="primary" v-model="drawer">
+      <v-list bg-color="primary">
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/9.jpg"
+        ></v-list-item>
+      </v-list>
+
+      <v-list density="compact" nav bg-color="primary">
+        <v-list-item
+          prepend-icon="mdi-home"
+          title="Home"
+          value="home"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account"
+          title="Account"
+          value="account"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-sticker-check"
+          title="Task"
+          value="Task"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-clock-time-eight"
+          title="Tempo"
+          value="check"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-text-box"
+          title="List"
+          value="list"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-cog"
+          title="Check"
+          value="check"
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <div class="m-10 bg-secondary rounded-pill" primary>test</div>
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-responsive max-width="156">
+        <v-text-field class="rounded-pill" label="Username"></v-text-field>
+      </v-responsive>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <template v-for="n in 4" :key="n">
+            <v-col class="mt-2" cols="12">
+              <strong>Category {{ n }}</strong>
+            </v-col>
+
+            <v-col v-for="j in 6" :key="`${n}${j}`" cols="6" md="2">
+              <div
+                class="pa-4 text-center bg-secondary rounded-xl rounded-be-0"
+              >
+                .rounded-xl .rounded-be-0
+              </div>
+            </v-col>
+          </template>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const drawer = ref(null);
+</script>
+<style scoped>
+.custom-text-field {
+  border-radius: 200px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,50 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+const app = createApp(App);
 
-createApp(App).mount('#app')
+// Vuetify
+import { createVuetify } from "vuetify";
+import 'vuetify/styles'
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import "@mdi/font/css/materialdesignicons.css";
+
+import "animate.css";
+
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    background: "#ffffff",
+    surface: "#ECEFF1",
+    primary: "#6200EE",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: "mdi",
+  },
+  theme: {
+    defaultTheme: "myCustomLightTheme",
+    themes: {
+      myCustomLightTheme,
+    },
+  },
+  // theme: {
+  //   defaultTheme: 'dark'
+  // }
+});
+
+app.use(vuetify);
+
+app.mount("#app");
